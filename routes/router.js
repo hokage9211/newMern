65f4391a69eef9eea5c5d20c6df913603637df26
aman <deswalaman9211@gmail.com>
2022-03-08 22:48:01 +0530

    updated router

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,9 @@
 const express=require("express")
 const router=express.Router()
-router.route("/home").get((req,res,next)=>{
+router.route("/home/:id").get((req,res,next)=>{
     res.status(200).json({
-        data:"successfull"
+        data:"successfull",
+        "ff":`${req.params}`
     })
 })
 module.exports=router
